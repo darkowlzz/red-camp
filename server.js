6c1582864ca9c252cb196a4812461b69818443a8
sunny <indiasuny000@gmail.com>
2015-04-10 14:28:19 +0530

    foo2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
 
 var port = process.env.PORT || 3000; //port set to 3000
 
-var uristring = process.env.MONGODB_URI || '';
+var uristring = 'mongodb://stepcamp:step000camp@ds047008.mongolab.com:47008/step';
 
 mongoose.connect(uristring, function (err, res) {
   if (err) {
