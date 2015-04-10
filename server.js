304c8cd4d1beb605bd40879c42d9fad14bcb4c9c
sunny <indiasuny000@gmail.com>
2015-04-10 15:24:37 +0530

    ugh

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ var express = require('express'),
 
 var port = process.env.PORT || 3000; //port set to 3000
 
-var uristring = process.env.MONGODB_URI || '';
-console.log('DB', uristring);
+//var uristring = process.env.MONGODB_URI || '';
+//console.log('DB', uristring);
 
-mongoose.connect(uristring, function (err, res) {
+mongoose.connect(process.env.MONGODB_URI, function (err, res) {
   if (err) {
     console.log('Error connecting to db', err);
   } else {
